2b30dab8e731e7148490ab63c27cafcacee52bc1
Tshepiso-coder <lesegograce5@gmail.com>
2025-10-02 00:04:40 +0200

    Add files via upload

---
+++ Chapter 6/chapter6.js
@@ -0,0 +1,49 @@
+//    var message = "Hello Tshepiso";
+//         console.log(message);
+//         //CONDITIONLS
+//         //IF STATEMENTS
+//         var firstname = "Vusi";
+//         console.log("Hello " + firstname);
+//         if(firstname.length >7){
+// console.log("Wow, you have a really long name!");
+//         }
+//         else{
+//             console.log("Your name isn't very long");
+//         }
+//short cut (highlight control + /)
+
+//CHAINING IF/ELSE STATEMENTS
+/*var lemonChicken = false;
+ var beefWithBlackBean = true;
+ var sweetAndSourPork = true;
+ if (lemonChicken) {
+ console.log("Great! I'm having lemon chicken!");
+ } else if (beefWithBlackBean) {
+ console.log("I'm having the beef.");
+ } else if (sweetAndSourPork) {
+ console.log("OK, I'll have the pork.");
+ } else {
+ console.log("Well, I guess I'll have rice then.");
+ };*/
+//  WHILE LOOPS
+// var sheepCounted = 0;
+// while (sheepCounted < 10){
+// console.log("I have counted" + sheepCounted + "sheep!");
+// sheepCounted++;
+// }
+// console.log("Zzzzzzzzzz");
+
+// for (var sheepCounted = 0; sheepCounted < 10; sheepCounted++){
+//     console.log("I have counted " + sheepCounted + " sheep!");
+//     }
+// //     console.log("Zzzzzzzz");
+
+//     var animals = ["Liom", "Flamingo","Polar Bear", "Boa Constrictor"];
+//     for (var i = 0; i < animals.length; i++){
+//         console.log("This zoo contains a " + animals[i] + ".")
+//         };
+
+//         var fname = "Nick";
+//         for (var i = 0; i < fname.length; i++){
+//             console.lo("My name contains the letter " + fname[i] + ".");
+//         }
