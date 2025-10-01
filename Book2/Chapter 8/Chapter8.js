bfddd50757e3d31a776fb6f363ae46a2500432f9
Tshepiso-coder <lesegograce5@gmail.com>
2025-10-02 00:27:39 +0200

    Add files via upload

---
+++ Book2/Chapter 8/Chapter8.js
@@ -0,0 +1,23 @@
+// typeof 8;
+// var doTheMath = 7 + 8 + 36 + 18 + 12;
+// typeof doTheMath;
+// typeof "the cat's favorite toy"
+// typeof automobile;
+
+ var dreamCar = {
+    make: "Oldsmobile",
+    model: "98",
+    color: "brown",
+    year: 1983,
+    bodyStyle: "Luxury Car",
+    price: 45000
+ }
+// alert("The type of dreamCar is : " + typeof dreamCat);
+ document.getElementById("pricetag").innerHTML = dreamCar.price;
+ document.getElementById("modelyear").innerHTML = dreamCar.year;
+ document.getElementById("body").style.backgroundColor = dreamCar.
+ color;
+ document.getElementById("body").innerHTML = dreamCar.make + " " + 
+dreamCar.model;
+
+
