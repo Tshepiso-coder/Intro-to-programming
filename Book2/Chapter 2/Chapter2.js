429b351724726f646c3455c1ba78f31ac4008124
Tshepiso-coder <lesegograce5@gmail.com>
2025-10-02 00:14:59 +0200

    Add files via upload

---
+++ Book2/Chapter 2/Chapter2.js
@@ -0,0 +1,11 @@
+//alert ("Coding is interesting")
+// for (var i = 0; i < 300; i++) {
+//  document.write("Coding is fun!");
+// ;}   
+// var element=document.create element("div");
+// element.textContent = "Testing";
+// document.body.append(element);
+
+ for (var i = 0; i < 300; i++) { 
+    document.write ("Joe said, \"Hi!\""); 
+};
---
+++ Book2/Chapter 3/chapter3.js
@@ -0,0 +1,90 @@
+// alert("chapter 3") 
+//---------------------Create a variables
+var book;
+book = "JavaScript For Kids Dummies";
+console.log(book);
+
+var myFirstname;
+var favoriteFood;
+var birthday;
+var timeOfDay;
+
+book = "The Call of the Wild";
+console.log(book);
+book.length
+
+//--------------------Data Types
+// var myName = "Tshepiso Molapisi";
+// var yourScore = 30;
+// alert("Hi, " + myName + ". Your current score is: " + 
+// yourScore);
+
+
+// var truck = {
+//     "color": ["Yellow", "Red"],
+//     price: 150000,
+//     "drive": function () {
+//         console.log("I am driving a big truck")
+//     }
+// };
+
+// console.log(truck.color);
+// console.log(truck["color"]);
+// truck.drive();
+
+//  document.write("How are you?<br>");
+//  document.write("I'm great! Thanks!<br>");
+//  document.write("That's awesome!");
+
+//  document.writeln("m");
+//  document.writeln("n");
+
+//  //Combining Input & Output
+//  var toName = prompt("Please enter recipient name: ");
+//  var fromName = prompt("Please enter sender's name");
+//   var letterBody = prompt("Compcse letter");
+
+//  document.write("Dear " + toName + ",<br><br>");
+//  document.write(letterBody + "<br><br>");
+//  document.write("Sincerely,<br>");
+//  document.write(fromName);
+
+
+ //================ Creating varibles
+
+// var book;
+
+// var myFirstname;
+// var favoriteFoods;
+// var birthday;
+// var timeOfDay;
+
+// var _petName;
+// var $schoolSubject;
+
+// var friends_Age;
+// var street$adress;
+
+//  //Results in error(Reserved keyword)
+// //  var break = "Tomorrow";
+// //  var switch = "Today";
+// //================ Storing data in variables
+
+// var book;
+// book = "JavaScript For Kids For Dummies";
+// console.log(book);
+
+// book = "The Call of the Wild";
+// console.log(book);
+// //================= Understanding Data Types
+
+// var book;
+// book = "JavaScript For Kids For Dummies";
+// console.log(book.length);
+// console.log("JavaScript For Kids For Dummies".length);
+
+// var string = "I am a string".indexOf("am");
+// console.log(string);
+
+// var string1 = "I am a string".indexOf("I");
+// console.log(string1);
