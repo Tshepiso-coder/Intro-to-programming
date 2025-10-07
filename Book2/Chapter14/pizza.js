6eeb91b95dcea6eb5019326b6bc80b32096a35d1
Tshepiso-coder <lesegograce5@gmail.com>
2025-10-07 12:50:31 +0200

    Add files via upload

---
+++ Book2/Chapter14/pizza.js
@@ -0,0 +1,36 @@
+// var language = prompt("What language do you speak?");
+//  if (language === "JavaScript") {
+//   alert("Great! Let's talk JavaScript!");
+//  } else {
+//   alert("I don't know what you're saying.");
+//  }
+
+
+
+//  var language = prompt("What language do you speak?");
+//  if (language === "JavaScript") {
+//   alert("Great! Let's talk JavaScript!");
+//   var speaksJavaScript = true;
+//  } else {
+//   alert("I don't know what you're saying.");
+//  }
+//  if (speaksJavaScript) {
+//   alert("It's great to meet you.");
+//  }
+
+//AND OPERATOR &&
+ if ((deliveryCity === "Anytown") && (orderPrice > 10)) {
+        var deliveryPrice = 0;
+    } else {
+        var deliveryPrice = 5;
+    }
+
+
+//OR OPERATOR ||
+ if (((deliveryCity === "Anytown") && (orderPrice > 10)) || 
+(birthday === "yes")) {
+        var deliveryPrice = 0;
+    } else {
+        var deliveryPrice = 5;
+    };
+
